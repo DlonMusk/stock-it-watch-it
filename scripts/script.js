909a7242f091a03a135249cc5c87001b22dc5c80
Tony Fan <tony.bs.0303@gmail.com>
2022-06-02 13:45:52 -0400

    styled news

--- scripts/script.js
+++ scripts/script.js
@@ -54,41 +54,57 @@ const autoCompleteSearch = (event) => {
 
 //_____________________________________________________________________________________________________________________________________________
 
+
+
 //_____________________________________________________________________________________________________________________________________________
 // TONY's CODES (DONT FUCK WITH THIS)
 function GRABNEWS() {
-    var requestURL = 'https://stocknewsapi.com/api/v1/category?section=general&items=9&page=1&token=rvngq2ylyqtk4trw2lpddxrqsfdndxsw1yrea2eh'
-    fetch(requestURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        news = data.data;
-        console.log(news);
-        for (var i = 0; i < news.length; i++) {
-            var newsURL = news[i].news_url;
-            var imageURL = news[i].image_url;
-            var newsSent = news[i].sentiment;
-            var newsSource = news[i].source_name;
-            var newsTitle = news[i].title;
-            var newsText = news[i].text;
-            var newsType = news[i].type;
-            $(".currentNews").append(
-                `<div class="newscard mt-3">
+  var requestURL = 'https://stocknewsapi.com/api/v1/category?section=general&items=9&page=1&token=rvngq2ylyqtk4trw2lpddxrqsfdndxsw1yrea2eh'
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      news = data.data;
+      console.log(news);
+      $(".currentNews").empty();
+      for (var i = 0; i < news.length; i++) {
+          var newsURL = news[i].news_url;
+          var newsDate = news[i].date.substring(0,22);
+          var imageURL = news[i].image_url;
+          var newsSent = news[i].sentiment;
+          var sentColor = "";
+          if (newsSent == "Neutral") {
+            var sentColor = "sentOrange";
+          } else if (newsSent == "Negative") {
+            var sentColor = "sentRed";
+          } else if (newsSent == "Positive") {
+            var sentColor = "sentGreen";
+          }
+          var newsSource = news[i].source_name;
+          var newsTitle = news[i].title;
+          var texttext = news[i].text;
+          var textLength = 100;
+          var newsText = texttext.substring(0, textLength) + "...";
+          var newsType = news[i].type;
+          $(".currentNews").append(
+              `<div class="newscard mt-3">
 
-                 <a target="_blank" href="${newsURL}" class="imagePreview"><img src="${imageURL}" class="newsImage"></a>
-                 <div class="titlesearch"><a target="_blank" href="${newsURL}">${newsTitle}</a></div>
-
-
-                </div>`
-            );
-        }
-      })
+               <div target="_blank" href="${newsURL}" class="imagePreview"><img src="${imageURL}" class="newsImage"></div>
+               <div class="titlesearch lead"><a target="_blank" href="${newsURL}">${newsTitle}</a> <hr></div>
+               <div class="textsearch"> ${newsSource} | ${newsDate} </div>
+               <div class="sentiment"> 
+                <div class="${sentColor}">${newsSent}</div>
+               </div>
+              </div>`
+          );
+      }
+    })
 };
 GRABNEWS();
-
-
 //____________________________________________________________________________________________________________________________________________ 
+
 
 // event listeners
 stockSearchEl.on('keyup', autoCompleteSearch);
+
